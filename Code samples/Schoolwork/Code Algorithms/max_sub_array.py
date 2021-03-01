6968c37f40b43c77fbfa09199f9dbdc32353339a
AshleyZhao1 <ash_spider@hotmail.com>
2021-03-01 02:37:06 +0000

    Adding some files

---
+++ Code samples/Schoolwork/Code Algorithms/max_sub_array.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+print("test")
+
+# Creates a random array
+def rand_array():
+  import random
+  temp_size = random.randint(1,6)
+  arr = []
+
+  for i in range(0, temp_size):
+    temp_elem = random.randint(-10,9)
+    arr.append(temp_elem)
+  
+  return arr
+
+
+# Brute-forces through the array
+def brute_force(arr, index=0):
+
+  i = index # 'focal' index of the array
+
+  # Debug print
+  print("Begin loop")
+
+  # Debug print
+  # print("i:"+str(i))
+
+  max_sum = 0
+
+  j = 0
+
+  temp_arr = list(arr)
+  temp_arr.pop(i)
+
+  while (j < len(arr)):
+
+
+    # Should change this to if sum + temp_arr[j] >= max_sum
+    temp_sum = arr[i] + sum(temp_arr[j:(len(arr) - 1)])
+    if (temp_sum > max_sum):
+      max_sum = temp_sum
+    
+    # Debug print
+    print("i: "+str(i))
+    print("j: "+str(j))
+    print("arr[i]: "+str(arr[i]))
+    print("temp_arr: ")
+    print(temp_arr)
+    print("slice: ")
+    print(temp_arr[j:(len(arr) - 1)])
+    print("max_sum: "+str(max_sum))
+
+    j = j + 1
+
+  if (i+1 >= len(arr)):
+    return max_sum
+  else:
+    temp_sum = brute_force(arr,i+1)
+    if (temp_sum > max_sum):
+      return temp_sum
+    else:
+      return max_sum
+
+
+
+
+arr = [3, 9, 8, -8, -1, 8]
+print(arr)
+print(brute_force(arr,0))
+print(arr)
